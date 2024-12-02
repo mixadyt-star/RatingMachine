f6302c3184ab679932d8e0d54a0abf100c462f46
mixadyt-star <riseofkingdoms10kkmoschi@gmail.com>
2024-12-02 13:57:58 +0300

    Added error handler

--- app/server.py
+++ app/server.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, session, render_template, send_file
 from threading import Thread
+import json
 
+from utils import email_worker, security, web_errors
 from documents import generator
-from utils import email_worker, security
 from data import database
 import config
 
@@ -15,7 +16,8 @@ if (secret_code_app == None):
 
 app.secret_key = secret_code_app
 
-def init_page(target_page, variables = None):
+def init_page(target_page, variables = {}):
+    variables["login"] = session.get("login", False)
     return render_template("default.html", target_page = target_page, variables = variables)
 
 @app.route('/', methods = ["GET"])
@@ -23,32 +25,52 @@ def main_page():
     return init_page("main.html")
 
 @app.route("/login", methods = ["GET", "POST"])
+@web_errors.ErrorChecker
 def login():
     variables = {
         "server_origin": config.SERVER_ORIGIN
     }
 
     if (request.method == "GET"):
-        return init_page("login.html", variables=variables)
+        if (session.get("login", False)):
+            return (1, web_errors.PAGE, "Вы уже вошли в аккаунт", None) # AccessError
+        
+        return (0, web_errors.PAGE, '', init_page("login.html", variables=variables))
     
     elif (request.method == "POST"):
-        data = request.json
-        if (data["type"] == "Child"):
-            user_data = database.search_for_child(data["email"])
-        elif (data["type"] == "Teacher"):
-            user_data = database.search_for_teacher(data["email"])
+        if (session.get("login", False)):
+            return (1, web_errors.JSON, "Вы уже вошли в аккаунт", None) # AccessError
 
-        if (user_data):
-            if (user_data[2] == security.hash(data["password"])):
-                session["email"] = data["email"]
-                session["password"] = data["password"]
+        try:
+            user_data = json.loads(request.data)
+        except json.JSONDecodeError:
+            return (2, web_errors.JSON, "Не удалось распаковать запрос", None) # JsonDecodeError
+        
+        user_type = user_data.get("type", '')
+        user_email = user_data.get("email", '')
+        user_password = user_data.get("password", '')
+        if (user_type == None or user_email == None or user_password == None):
+            return (3, web_errors.JSON, "Заполнены не все поля", None)
+
+        if (user_type == "Child"):
+            user_data_loaded = database.search_for_child(user_email)
+        elif (user_type == "Teacher"):
+            user_data_loaded = database.search_for_teacher(user_email)
+        else:
+            return (4, web_errors.JSON, "Некорректный запрос", None) # TypeNotCorrectError
+
+        if (user_data_loaded):
+            user_password_loaded = user_data_loaded[2]
+            if (user_password_loaded == security.hash(user_password)):
+                session["email"] = user_email
+                session["password"] = user_password
                 session["login"] = True
 
-                return {"login": True, "error_message": ""}
+                return (0, web_errors.JSON, '', {"login": True})
             else:
-                return {"login": False, "error_message": "Неправильный пароль или почта аккаунта"}
+                return (5, web_errors.JSON, "Неправильный пароль или почта аккаунта", None) # UserOrPasswordWrongError
         else:
-            return {"login": False, "error_message": "Неправильный пароль или почта аккаунта"}
+            return (5, web_errors.JSON, "Неправильный пароль или почта аккаунта", None) # UserOrPasswordWrongError
 
 @app.route("/register", methods = ["GET"])
 def register():
---
+++ app/utils/web_errors.py
@@ -0,0 +1,66 @@
+from flask import render_template
+
+JSON = 0
+PAGE = 1
+
+class WebError:
+    def __init__(self, error_code: int):
+        self.error_code = error_code
+
+    def get_json(self, error: bool = True, message: str | None = None, payload: dict | list | int | str | None = None):
+        return {
+            "error": error,
+            "error_code": self.error_code,
+            "error_message": message,
+            "payload": payload
+        }
+
+    def get_page(self, message: str):
+        return render_template("error_page.html", error_message = message)
+
+class AccessError(WebError):
+    def __init__(self):
+        super().__init__(error_code = 1)
+
+class JsonDecodeError(WebError):
+    def __init__(self):
+        super().__init__(error_code = 2)
+
+class FewValuesError(WebError):
+    def __init__(self):
+        super().__init__(error_code = 3)
+
+class TypeNotCorrectError(WebError):
+    def __init__(self):
+        super().__init__(error_code = 4)
+
+class UserOrPasswordWrongError(WebError):
+    def __init__(self):
+        super().__init__(error_code = 5)
+
+errors = {
+    1: AccessError,
+    2: JsonDecodeError,
+    3: FewValuesError,
+    4: TypeNotCorrectError,
+    5: UserOrPasswordWrongError
+}
+
+def ErrorChecker(func):
+    def inner():
+        error_code, type, message, payload = func()
+
+        if (error_code == 0):
+            if (type == JSON):
+                return WebError(0).get_json(error = False, payload = payload)
+            elif (type == PAGE):
+                return payload
+        else:
+            error = errors[error_code]()
+            if (type == JSON):
+                return error.get_json(message = message, payload = payload)
+            elif (type == PAGE):
+                return error.get_page(message)
+
+    inner.__name__ = func.__name__
+    return inner
