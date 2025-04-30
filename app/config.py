a8150215ee98e6af19169cb6017aa49b6fe5203f
mixadyt <riseofkingdoms10kkmoschi@gmail.com>
2025-04-30 07:59:55 +0300

    patch 05

---
+++ app/config.py
@@ -0,0 +1,15 @@
+# Mail config
+SENDER_EMAIL = "ваша почта"
+SENDER_ALIAS = "SuperVision"
+SENDER_PASSWORD = "ваш 16-ти значный пароль от почты"
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 465
+
+# Recognition config
+MAX_CELL_WIDTH = 250
+MIN_CELL_WIDTH = 100
+MAX_CELL_HEIGHT = 70
+MIN_CELL_AREA = 1000
+
+# Access
+REGISTRATION_ENABLED = True
